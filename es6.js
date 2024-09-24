cb8de1b57fbdded5001b34654b094f10ee22a8bb
RJ Trenchard <rjtrenchard@gmail.com>
2024-09-24 14:41:03 -0700

    add es6.js

---
+++ es6.js
@@ -0,0 +1,15 @@
+const r = 100;
+const g = 200;
+const b = 255;
+
+const message = `color is ${r + g + b}`;
+
+console.log(message);
+
+const n = function () { 
+    return "test";
+}
+
+const new_message = `test function = ${n()}`;
+
+console.log(new_message);
